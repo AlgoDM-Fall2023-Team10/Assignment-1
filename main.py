da0b37f9072d16d2ca9916ba2ba337f955846c8d
surya-teja-karri <karri.s@northeastern.edu>
2023-10-07 11:02:37 -0400

    added basic import statements and snowflake login

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-print("hello world")
+import streamlit as st
+from sqlalchemy import create_engine, text, bindparam
+import pandas as pd
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+# Streamlit UI
+st.title("Snowflake SQL Query Tool")
+
+
+# Load environment variables from .env
+load_dotenv()
+
+# Retrieve Snowflake credentials from environment variables
+SNOWFLAKE_username = os.getenv("SNOWFLAKE_username")
+SNOWFLAKE_password = os.getenv("SNOWFLAKE_password")
+SNOWFLAKE_account_identifier = os.getenv("SNOWFLAKE_account_identifier")
+
+# Create a Snowflake connection URL
+snowflake_connection_url = f"snowflake://{SNOWFLAKE_username}:{SNOWFLAKE_password}@{SNOWFLAKE_account_identifier}"
+snowflake_engine = create_engine(snowflake_connection_url)
 
 
 
